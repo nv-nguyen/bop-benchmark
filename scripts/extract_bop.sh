c6f4be239cff9aad38a3c15cd65947c5ce8b75d9
Van Nguyen Nguyen <vanngn.nguyen@gmail.com>
2024-04-18 13:39:44 +0900

    Update extract_bop.sh

--- scripts/extract_bop.sh
+++ scripts/extract_bop.sh
@@ -1,8 +1,19 @@
 for file in $LOCAL_DIR/$DATASET_NAME/*.zip; do
     if [[ $file == *_base.zip ]]; then
-        unzip -j "$file" -d "$(dirname "$file")"
+        unzip -q -j "$file" -d "$(dirname "$file")"
+        echo "Extraction of base file $file is complete."
+    elif [[ $file == *_test_bop19.zip ]]; then
+        test_all_file="${file/_test_bop19/_test_all}"
+        if [[ -f $test_all_file ]]; then
+            echo "Skipping $file as $test_all_file exists."
+        else
+            echo "Unzipping $file..."
+            unzip -q "$file" -d "$(dirname "$file")"
+            echo "Extraction of $file is complete."
+        fi
     else
-        unzip "$file" -d "$(dirname "$file")"
+        echo "Unzipping $file..."
+        unzip -q "$file" -d "$(dirname "$file")"
+        echo "Extraction of $file is complete."
     fi
 done
-
